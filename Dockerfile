db7614adc9120a12ac4dfd5e3000b06389797d52
Michal Negacz <michal@negacz.net>
2018-06-01 19:41:32 +0200

    Registering using file list and renewing

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,12 @@ FROM certbot/certbot
 
 VOLUME /etc/letsencrypt
 
-ENV DOMAIN example.org
 ENV EMAIL example@example.org
 
 COPY auth /usr/local/bin/
 COPY clean /usr/local/bin/
 COPY ovh.conf /etc/
+COPY domains.txt /
 COPY entrypoint.sh /
 
 ENTRYPOINT /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,38 @@
 #/bin/sh
 set -e
 
-certbot \
-        --agree-tos \
-        --non-interactive \
-        --manual \
-        --manual-auth-hook /usr/local/bin/auth \
-        --manual-cleanup-hook /usr/local/bin/clean \
-        --domain $DOMAIN \
-        --domain *.$DOMAIN \
-        --email $EMAIL \
-        --preferred-challenges dns-01 \
-        --server https://acme-v02.api.letsencrypt.org/directory \
-        --renew-by-default \
-        --expand \
-        --manual-public-ip-logging-ok \
-        certonly
+register() {
+  DOMAIN=$1
+  echo "Registering $DOMAIN"
+  certbot \
+    --agree-tos \
+    --non-interactive \
+    --manual \
+    --manual-auth-hook /usr/local/bin/auth \
+    --manual-cleanup-hook /usr/local/bin/clean \
+    --domain $DOMAIN \
+    --domain *.$DOMAIN \
+    --email $EMAIL \
+    --preferred-challenges dns-01 \
+    --server https://acme-v02.api.letsencrypt.org/directory \
+    --renew-by-default \
+    --expand \
+    --manual-public-ip-logging-ok \
+    certonly
+}
+
+renew() {
+  echo "Renewing"
+  certbot \
+    --manual-auth-hook /usr/local/bin/auth \
+    --manual-cleanup-hook /usr/local/bin/clean \
+    renew
+}
+
+while read DOMAIN; do
+  if [ ! -d "/etc/letsencrypt/live/$DOMAIN" ]; then
+    register "$DOMAIN"
+  fi
+done </domains.txt
+
+renew
